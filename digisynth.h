0c7505255684a0bf102bd3887bd2230d82aa5d72
D4p0up <dapoup@hotmail.com>
2018-03-25 17:48:26 +0200

    Code cleaning

--- digisynth.h
+++ digisynth.h
@@ -1,24 +1,24 @@
 // LEDS shift 74HC595
-#define D1    1
-#define D2    2
-#define D3    4
-#define D4    8
-#define D5    16
-#define D6    32
-#define D7    64
-#define D8    128
+#define D1      1
+#define D2      2
+#define D3      4
+#define D4      8
+#define D5      16
+#define D6      32
+#define D7      64
+#define D8     128
 // LEDS Pins
-#define D11   4
-#define D10   5
-#define D9    6
+#define D11     4
+#define D10     5
+#define D9      6
 // Switchs Pins
-#define SW1   7
-#define SW2   9
-#define SW3   8
-#define SW4   12
-#define SW5   2
-#define SW6   3
+#define SW1     7
+#define SW2     9
+#define SW3     8
+#define SW4     12
+#define SW5     2
+#define SW6     3
 // SPI
-#define SLATCH 10
-#define SMOSI 11
-#define SDATA 13
+#define SLATCH  10  // Teensy : SS     595 : 12_RCLK
+#define SMOSI   11  // Teensy : MOSI   595 : 14_SER
+#define SCLK    13  // Teensy : CLK    595 : 11_SRCLK
--- fw10.ino
+++ fw10.ino
@@ -1,16 +1,41 @@
-/* DIGISYNTH Firmware
-*/
+/* DIGISYNTH Firmware V0.02*/
 
-#include <SPI.h>
 #include "digisynth.h"
 
-unsigned char cycle;
+// Global Variables
+unsigned char buttons;
 
+/*=============================================================================
+ * INIT : Called once at Startup
+ *=============================================================================*/
 void setup() {
-  // init SPI
-  SPI.begin();
+  pins_init();        // Setting Proper pin states
+  
+  
+  test_full_loop(10); // LED Pulses for Setup Exit
+}
+
+/*=============================================================================
+ * MAIN LOOP - Repeated
+ *=============================================================================*/
+void loop() {
+
+//test_full_loop(100);
+//test_led_loop(100);
+test_button();
+}
+
+
+// ___________________
+// Functions herebelow 
+
+/*=============================================================================
+ * pins_init : Sets Pins inputs and outputs at proper direction
+ *=============================================================================*/
+void pins_init(void) {
   pinMode(SLATCH, OUTPUT);
-  // initialize the digital pin as an output.
+  pinMode(SMOSI, OUTPUT);
+  pinMode(SCLK, OUTPUT);
   pinMode(D9, OUTPUT);
   pinMode(D10, OUTPUT);
   pinMode(D11, OUTPUT);
@@ -21,19 +46,49 @@ void setup() {
   pinMode(SW5, INPUT);
   pinMode(SW6, INPUT);          
 }
-
-// the loop() methor runs over and over again,
-// as long as the board has power
-
-void loop() {
-digitalWrite(SLATCH, HIGH);
-SPI.transfer(128);
-digitalWrite(SLATCH, LOW);
-//test_led_loop(100);
-test_button();
+/*=============================================================================
+ * test_full_loop : loops among the Whole LEDs
+ *=============================================================================*/
+void test_full_loop(unsigned int tdelay) {
+  digitalWrite(D11, HIGH);   
+  delay(tdelay);                 
+  digitalWrite(D11, LOW);
+  digitalWrite(D9, HIGH);   
+  delay(tdelay);                 
+  digitalWrite(D9, LOW);
+  digitalWrite(D10, HIGH);   
+  delay(tdelay);                 
+  digitalWrite(D10, LOW);
+  buttons = D8;
+  setbuttons();
+  delay(tdelay);
+  buttons = D1;
+  setbuttons();
+  delay(tdelay);
+  buttons = D2;
+  setbuttons();
+  delay(tdelay);
+  buttons = D3;
+  setbuttons();
+  delay(tdelay);
+  buttons = D4;
+  setbuttons();
+  delay(tdelay);
+  buttons = D5;
+  setbuttons();
+  delay(tdelay);
+  buttons = D6;
+  setbuttons();
+  delay(tdelay);
+  buttons = D7;
+  setbuttons();
+  delay(tdelay);
+  buttons = 0;
+  setbuttons();    
 }
-
-
+/*=============================================================================
+ * test_led_loop : loops among the 3 Direct LEDS D9, D10 and D11
+ *=============================================================================*/
 void test_led_loop(unsigned int tdelay) {
   digitalWrite(D9, HIGH);   
   delay(tdelay);                 
@@ -48,10 +103,19 @@ void test_led_loop(unsigned int tdelay) {
   digitalWrite(D11, LOW);
   delay(tdelay);
 }
-
+/*=============================================================================
+ * test_button : basic loop that reads switches and enables three direct LEDs
+ *=============================================================================*/
 void test_button(void) {
 if (digitalRead(SW1)&digitalRead(SW4)) digitalWrite(D9, LOW); else digitalWrite(D9, HIGH); 
 if (digitalRead(SW2)&digitalRead(SW5)) digitalWrite(D10, LOW); else digitalWrite(D10, HIGH); 
 if (digitalRead(SW3)&digitalRead(SW6)) digitalWrite(D11, LOW); else digitalWrite(D11, HIGH); 
 }
-
+/*=============================================================================
+ * setbuttons : Pushes 8 status LEDs from "buttons" global variable
+ *=============================================================================*/
+void setbuttons(void) {
+digitalWrite(SLATCH, LOW);
+shiftOut(SMOSI, SCLK, MSBFIRST, buttons);
+digitalWrite(SLATCH, HIGH);
+}
